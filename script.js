36135d92b48c9dfae2e81457a6afa55ed300c056
maestro-johan <js870709@gmail.com>
2026-09-14 18:02:07 +0200

    feat: else-if temperature zones

--- script.js
+++ script.js
@@ -44,3 +44,16 @@ console.log(product.inStock);
 for (const city of cities) {
   console.log(city);
 }
+
+
+console.log("------------------------------------------------------------------")
+
+const tempC = 12;
+
+if (tempC >= 20) {
+  console.log("varmt");
+} else if (tempC >= 10) {
+  console.log("ljummet");
+} else {
+  console.log("kallt");
+}
